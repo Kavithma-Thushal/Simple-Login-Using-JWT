432ce46ad4ab1784532718e231dad237cb14795c
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-08-01 23:14:31 +0530

    SignUp.html & SignIn.html Added

--- BackEnd/src/main/java/lk/ijse/gdse66/demo/controller/CustomerController.java
+++ BackEnd/src/main/java/lk/ijse/gdse66/demo/controller/CustomerController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
 @RequestMapping("api/v1/customer")
 public class CustomerController {
--- BackEnd/src/main/java/lk/ijse/gdse66/demo/controller/UserController.java
+++ BackEnd/src/main/java/lk/ijse/gdse66/demo/controller/UserController.java
@@ -6,11 +6,9 @@ import lk.ijse.gdse66.demo.security.auth.response.Token;
 import lk.ijse.gdse66.demo.service.AuthenticationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
 @RequestMapping("api/v1/auth")
 @RequiredArgsConstructor
